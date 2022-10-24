118e5a0979f57cd482d0e9c9ebdd2f8d77ea3aef
Helena Tenev <htenev14@gmail.com>
2022-10-24 12:41:51 -0400

    commit tree dd3aa3f7ae1458fda3707010d6d4cdd3c9302033

---
+++ index.js
@@ -0,0 +1,17 @@
+function add(a,b) {
+    return a + b;
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+function preserveDecimalString(string) {
+    return parseFloat(string);
+}
